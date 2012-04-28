b8d822b32688f84a40882b59472f96957e8e8e46
toran billups <toranb@gmail.com>
2012-04-28 15:59:51 -0500

    added easy_install and pip_install configuration

--- cookbooks/development/recipes/default.rb
+++ cookbooks/development/recipes/default.rb
@@ -3,3 +3,11 @@ node[:base_packages].each do |pkg|
     :upgrade
   end
 end
+
+node[:easy_install_packages].each do |pkg|
+  execute "easy_install #{pkg}"
+end
+
+node[:pip_packages].each do |pkg|
+  execute "pip install #{pkg}"
+end
