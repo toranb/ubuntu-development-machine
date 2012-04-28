3ae3fdca265461a91caf11652170bcbf28cc784a
toran billups <toranb@gmail.com>
2012-04-28 16:19:33 -0500

    added custom vim chef recipe

---
+++ cookbooks/development/recipes/vim.rb
@@ -0,0 +1,24 @@
+package "vim"
+package "vim-scripts"
+package "vim-doc"
+package "vim-latexsuite"
+package "vim-gui-common"
+package "vim-gnome"
+
+if node[:users]
+    node[:users].each_pair do |username, info|
+
+      home_dir = info[:home]
+      execute "rm -rf #{home_dir}.vimrc"
+      execute "rm -rf #{home_dir}.vim"
+
+      execute "cd #{home_dir} && git clone https://github.com/toranb/vimfiles.git #{home_dir}.vim"
+      execute "cd #{home_dir}.vim && rake"
+
+      execute "cd #{home_dir}.vim && mkdir -p _backup"
+      execute "cd #{home_dir}.vim && mkdir -p _temp"
+      execute "chmod 777 #{home_dir}.vim/_backup"
+      execute "chmod 777 #{home_dir}.vim/_temp"
+    end
+end
+
