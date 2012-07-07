bbe2a622b1e58b3e167c75a8a8530e6ae9d723e4
toran billups <toranb@gmail.com>
2012-07-07 09:13:12 -0500

    updated to node 0.8.1

--- cookbooks/development/recipes/node.rb
+++ cookbooks/development/recipes/node.rb
@@ -5,16 +5,16 @@ if node[:users]
       if node_installed.empty?
           home_dir = info[:home]
           node_dir = "#{home_dir}Downloads/"
-          node_download = "#{node_dir}joyent-node-v0.6.15-0-gf160a45.zip"
+          node_download = "#{node_dir}joyent-node-v0.8.1-0-g2134aa3.zip"
           node_extracted_dir = "#{node_dir}joyent-node/"
 
           execute "rm -rf #{node_download}"
           execute "rm -rf #{node_extracted_dir}"
 
-          execute "wget 'https://github.com/joyent/node/zipball/v0.6.15' -O #{node_download}"
+          execute "wget 'https://github.com/joyent/node/zipball/v0.8.1' -O #{node_download}"
           execute "unzip #{node_download} -d #{node_extracted_dir}"
            
-          execute "cd #{node_extracted_dir}joyent-node-d90d46f/ && ./configure && make && make install"
+          execute "cd #{node_extracted_dir}joyent-node-fd97767/ && ./configure && make && make install"
 
           execute "npm install jasmine-node -g"
           execute "npm install jsdom"
