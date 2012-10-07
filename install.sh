b6f072b41fdeaf5789c937cf5756c588d2acb0c5
Daniel Vizzini <dvizzini@gmail.com>
2012-10-07 09:55:10 -0700

    tweaking install script

--- install.sh
+++ install.sh
@@ -6,9 +6,15 @@
 
 RUBY_VERSION="1.9.3"
 LOG_FILE="./install.log"
-echo "Installer started"
 
-# Installing Dependencies
+# check arguments
+json="${1}"
+
+if [ -z "$json" ]; then
+    echo "Usage: ./install.sh [json]"
+    exit
+fi
+
 echo ".............Updating package cache..........."
 sudo apt-get update -o Acquire::http::No-Cache=True >$LOG_FILE
 
@@ -16,32 +22,29 @@ echo "............Preparing RVM Install............."
 sudo apt-get -y install build-essential curl zlib1g-dev libreadline-dev libssl-dev libxml2-dev openssl libyaml-dev libopenssl-ruby1.9.1 ruby1.9.1 rubygems1.9.1 ruby1.9.1-dev >>$LOG_FILE
 export PATH=$PATH:/var/lib/gems/1.9.1/bin:/usr/lib/ruby/1.9.1/
 
-# Install RVM (Ruby Version Manager)
-echo "................Installing RVM................."
+echo "................Installing RVM................"
 curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable >>$LOG_FILE
 
-echo ".................Loading RVM...................."
+echo ".................Loading RVM.................."
 source ~/.rvm/scripts/rvm >>$LOG_FILE
 
 # Exiting sudo mode
 
-# Install Ruby v 1.8.7
-echo ".............Installing Ruby v 1.8.7............."
+echo "...........Installing Ruby v 1.8.7............"
 rvm install 1.8.7 >>$LOG_FILE
 
-# Install Latest version of Ruby
-echo ".............Installing Ruby v $RUBY_VERSION............."
+echo "...........Installing Ruby v $RUBY_VERSION............"
 rvm install $RUBY_VERSION >>$LOG_FILE
 
-echo "........Setting the default version of Ruby......."
+echo ".....Setting the default version of Ruby......"
 rvm --default use $RUBY_VERSION >>$LOG_FILE
 
-echo "................Installing Chef..................."
+echo "...............Installing Chef................"
 gem install ruby-shadow chef --no-rdoc --no-ri >>$LOG_FILE
 
 # Back into sudo
 
-echo "...............Following Recipes.................."
+echo ".............Following Recipes................"
 `which chef-solo` --config solo.rb --json-attributes $json >>$LOG_FILE
 
 sudo reboot now
