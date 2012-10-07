6631d3f04747acbb97b60e09b55430ba194f1cac
Daniel Vizzini <dvizzini@gmail.com>
2012-10-07 09:35:27 -0700

    tweaking install script

--- install.sh
+++ install.sh
@@ -6,52 +6,42 @@
 
 RUBY_VERSION="1.9.3"
 LOG_FILE="./install.log"
-DONE=".....................Done......................"
 echo "Installer started"
 
 # Installing Dependencies
 echo ".............Updating package cache..........."
-sudo apt-get update -o Acquire::http::No-Cache=True >>$LOG_FILE
-echo $DONE
+sudo apt-get update -o Acquire::http::No-Cache=True >$LOG_FILE
 
 echo "............Preparing RVM Install............."
 sudo apt-get -y install build-essential curl zlib1g-dev libreadline-dev libssl-dev libxml2-dev openssl libyaml-dev libopenssl-ruby1.9.1 ruby1.9.1 rubygems1.9.1 ruby1.9.1-dev >>$LOG_FILE
 export PATH=$PATH:/var/lib/gems/1.9.1/bin:/usr/lib/ruby/1.9.1/
-echo $DONE
 
 # Install RVM (Ruby Version Manager)
 echo ".................Installing RVM.................."
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer) >>$LOG_FILE
-echo $DONE
+(bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer) >>$LOG_FILE
 
 echo "..................Loading RVM...................."
 source ~/.rvm/scripts/rvm >>$LOG_FILE
-echo $DONE
 
 # Exiting sudo mode
 
 # Install Ruby v 1.8.7
 echo ".............Installing Ruby v 1.8.7............."
 rvm install 1.8.7 >>$LOG_FILE
-echo $DONE
 
 # Install Latest version of Ruby
 echo ".............Installing Ruby v $RUBY_VERSION............."
 rvm install $RUBY_VERSION >>$LOG_FILE
-echo $DONE
 
 echo "........Setting the default version of Ruby......."
 rvm --default use $RUBY_VERSION >>$LOG_FILE
-echo $DONE
 
 echo "................Installing Chef..................."
 gem install ruby-shadow chef --no-rdoc --no-ri >>$LOG_FILE
-echo $DONE
 
 # Back into sudo
 
 echo "...............Following Recipes.................."
 `which chef-solo` --config solo.rb --json-attributes $json >>$LOG_FILE
-echo $DONE
 
 sudo reboot now
