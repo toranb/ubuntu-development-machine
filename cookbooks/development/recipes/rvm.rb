97c71e170b38713367614b7a82992f4d537a5423
Daniel Vizzini <dvizzini@gmail.com>
2012-10-10 12:42:58 -0700

    fixed misspelling

--- cookbooks/development/recipes/rvm.rb
+++ cookbooks/development/recipes/rvm.rb
@@ -2,13 +2,12 @@ if node[:users]
   node[:users].each_pair do |username, info|
 
 	rvm_installed = `which rvm`
-
 	if rvm_installed.empty?
           home_dir = info[:home]
           default_rvm = node[:default_ruby_version]
           execute "su -c 'bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)' #{username}"
           execute "su -c 'source #{home_dir}/.rvm/scripts/rvm' #{username}"
-          node[:ruby_versions].each_value di |version_number|
+          node[:ruby_versions].each_value do |version_number|
             execute "su -c 'rvm install #{version_number}' #{username}" 
           end
 	  execute "su -c 'rvm --default use #{default_rvm}' #{username}"
