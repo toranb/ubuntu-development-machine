08d0b8ed83eb811c5238a06675590d8e23bce2b7
Daniel Vizzini <dvizzini@gmail.com>
2012-10-10 12:51:23 -0700

    fixed error

--- cookbooks/development/recipes/rvm.rb
+++ cookbooks/development/recipes/rvm.rb
@@ -7,7 +7,7 @@ if node[:users]
           default_rvm = node[:default_ruby_version]
           execute "su -c 'bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)' #{username}"
           execute "su -c 'source #{home_dir}/.rvm/scripts/rvm' #{username}"
-          node[:ruby_versions].each_value do |version_number|
+          node[:ruby_versions].each do |version_number|
             execute "su -c 'rvm install #{version_number}' #{username}" 
           end
 	  execute "su -c 'rvm --default use #{default_rvm}' #{username}"
