934679485cda6ccfe41ce2a1c41de95fd6095e46
toran billups <toranb@gmail.com>
2013-03-07 07:06:27 -0600

    updated chrome recipe to be rerun friendly

--- cookbooks/development/recipes/chrome.rb
+++ cookbooks/development/recipes/chrome.rb
@@ -1,4 +1,7 @@
-execute "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -"
-execute "sudo sh -c 'echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'"
-execute "apt-get update"
-execute "apt-get install google-chrome-stable -y"
+chrome_installed = `which google-chrome`
+if chrome_installed.empty?
+    execute "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -"
+    execute "sudo sh -c 'echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'"
+    execute "apt-get update"
+    execute "apt-get install google-chrome-stable -y"
+end
