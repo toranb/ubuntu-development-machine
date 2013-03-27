292bbd944ad0a02495785f9d38c8a70117dbbdc8
toran billups <toranb@gmail.com>
2013-03-27 18:36:04 -0500

    updates to the base settings / rvm recipe

--- cookbooks/development/recipes/rvm.rb
+++ cookbooks/development/recipes/rvm.rb
@@ -4,7 +4,7 @@ if node[:users]
 	rvm_installed = `which rvm`
 	if rvm_installed.empty?
           home_dir = info[:home]
-          execute "su -c 'bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)' #{username}"
+          execute "su -c 'bash -s stable --rails --autolibs=enabled < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)' #{username}"
           execute "su -c 'source #{home_dir}.rvm/scripts/rvm' #{username}"
           node[:ruby_versions].each do |version_number|
             execute "su -c '#{home_dir}.rvm/bin/rvm install #{version_number}' #{username}" 
