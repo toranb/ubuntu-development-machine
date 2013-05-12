83f7b0892603e2ff830c74262b21a6765f232fae
toran billups <toranb@gmail.com>
2013-05-11 21:20:49 -0500

    node.js will now be the latest (not 0.8.9)

--- cookbooks/development/recipes/node.rb
+++ cookbooks/development/recipes/node.rb
@@ -1,17 +1,12 @@
 if node[:users]
   node[:users].each_pair do |username, info|
 
-	node_installed = `which node`
+	node_installed = `which npm`
 	if node_installed.empty?
-	  home_dir = info[:home]
-	  node_dir = "#{home_dir}Downloads/"
-	  node_download = "#{node_dir}joyent-node-v0.8.9-0-gb88c390.zip"
-	  node_extracted_dir = "#{node_dir}joyent-node/"
-
-	  execute "wget 'https://github.com/joyent/node/zipball/v0.8.9' -O #{node_download}"
-	  execute "unzip #{node_download} -d #{node_extracted_dir}"
-	   
-	  execute "cd #{node_extracted_dir}joyent-node-a5726e8/ && ./configure && make && make install"
+        execute "apt-get install python-software-properties python g++ make -y"
+        execute "add-apt-repository ppa:chris-lea/node.js"
+        execute "apt-get update"
+        execute "apt-get install nodejs -y"
 	end
 
   end
